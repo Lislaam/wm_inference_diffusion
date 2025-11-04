f67f1cfea6d27c3aa091c1563bf00e8e51e6c0f1
Labeebah. <Mona Lisa.>
2025-11-04 13:14:18 +0000

    move files

--- run_all.sh
+++ run_all.sh
@@ -61,12 +61,12 @@ run_experiment() {
 # ---------------------------
 # First loop
 # ---------------------------
-for env_type in Alien Amidar Assault Asterix BankHeist BattleZone Breakout ChopperCommand CrazyClimber DemonAttack Freeway Frostbite Gopher Hero Jamesbond Kangaroo Krull KungFuMaster MsPacman Pong PrivateEye Qbert RoadRunner Seaquest UpNDown; do
-  for planning_steps in 5; do
-    for inner_planning_steps in 1; do
-      for entropy_threshold in 1.5; do
-        for planning_mode in reward value; do
-          for seed in 0 1 2; do
+for env_type in Alien #Amidar Assault Asterix BankHeist BattleZone Breakout ChopperCommand CrazyClimber DemonAttack Freeway Frostbite Gopher Hero Jamesbond Kangaroo Krull KungFuMaster MsPacman Pong PrivateEye Qbert RoadRunner Seaquest UpNDown; do
+  for planning_steps in 0; do
+    for inner_planning_steps in 0; do
+      for entropy_threshold in 2; do
+        for planning_mode in reward; do
+          for seed in 0; do
             run_experiment \
               env.env_type=$env_type \
               evaluation.planning_steps=$planning_steps \
