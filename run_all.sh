919d019799294b696aeebd61ad2fb4dca2158964
Labeebah. <Mona Lisa.>
2026-01-02 14:26:43 +0000

    test

--- run_all.sh
+++ run_all.sh
@@ -54,7 +54,7 @@ run_experiment() {
 # -------------------------------------------------------
 # MAIN LOOP
 # -------------------------------------------------------
-for env_type in Alien Amidar Assault Asterix BankHeist BattleZone Breakout ChopperCommand CrazyClimber DemonAttack Freeway Frostbite Gopher Hero Jamesbond Kangaroo Krull KungFuMaster MsPacman Pong PrivateEye Qbert RoadRunner Seaquest UpNDown; do #
+for env_type in Alien; do # Amidar Assault Asterix BankHeist BattleZone Boxing Breakout ChopperCommand CrazyClimber DemonAttack Freeway Frostbite Gopher Hero Jamesbond Kangaroo Krull KungFuMaster MsPacman Pong PrivateEye Qbert RoadRunner Seaquest UpNDown; do #
   for planning_steps in 0; do
     for inner_planning_steps in 0; do
       for entropy_threshold in 2; do
