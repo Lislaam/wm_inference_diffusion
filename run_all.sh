0a1a6df1ece0e9242c767071effae7537a8d682f
Labeebah. <Mona Lisa.>
2026-01-13 14:59:54 +0000

    key error

--- run_all.sh
+++ run_all.sh
@@ -57,7 +57,7 @@ run_experiment() {
 for env_type in Alien; do # Amidar Assault Asterix BankHeist BattleZone Boxing Breakout ChopperCommand CrazyClimber DemonAttack Freeway Frostbite Gopher Hero Jamesbond Kangaroo Krull KungFuMaster MsPacman Pong PrivateEye Qbert RoadRunner Seaquest UpNDown; do #
   for planning_steps in 0; do
     for inner_planning_steps in 0; do
-      for entropy_threshold in 2; do
+      for entropy_threshold_sigma in 1; do
         for planning_mode in reward; do
           for seed in 0 1 2; do
 
@@ -65,7 +65,7 @@ for env_type in Alien; do # Amidar Assault Asterix BankHeist BattleZone Boxing B
               env.env_type="$env_type" \
               evaluation.planning_steps="$planning_steps" \
               evaluation.inner_planning_steps="$inner_planning_steps" \
-              evaluation.entropy_threshold="$entropy_threshold" \
+              evaluation.entropy_threshold_sigma="$entropy_threshold_sigma" \
               evaluation.planning_mode="$planning_mode" \
               evaluation.planning_depth=2 \
               common.seed="$seed" \
