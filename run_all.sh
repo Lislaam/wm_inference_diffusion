d75cb872533fbee86da380dd3856c13b24062a2e
Labeebah. <Mona Lisa.>
2026-01-14 19:27:22 +0000

    next

--- run_all.sh
+++ run_all.sh
@@ -179,29 +179,29 @@ run_experiment() {
 # done
 
 
-for env_type in BattleZone; do # Breakout ChopperCommand CrazyClimber DemonAttack Freeway Frostbite Gopher Hero Jamesbond Kangaroo Krull KungFuMaster MsPacman Pong PrivateEye Qbert RoadRunner Seaquest UpNDown
-  for planning_steps in 5; do
-    for inner_planning_steps in 1; do
-      for entropy_threshold in 4.15; do
-        for planning_mode in value reward; do
-          for seed in 0 1 2; do
+# for env_type in BattleZone; do # Breakout ChopperCommand CrazyClimber DemonAttack Freeway Frostbite Gopher Hero Jamesbond Kangaroo Krull KungFuMaster MsPacman Pong PrivateEye Qbert RoadRunner Seaquest UpNDown
+#   for planning_steps in 5; do
+#     for inner_planning_steps in 1; do
+#       for entropy_threshold in 4.15; do
+#         for planning_mode in value reward; do
+#           for seed in 0 1 2; do
 
-            run_experiment \
-              env.env_type="$env_type" \
-              evaluation.planning_steps="$planning_steps" \
-              evaluation.inner_planning_steps="$inner_planning_steps" \
-              evaluation.entropy_threshold="$entropy_threshold" \
-              evaluation.planning_mode="$planning_mode" \
-              evaluation.planning_depth=2 \
-              common.seed="$seed" \
-              wandb.mode=online
+#             run_experiment \
+#               env.env_type="$env_type" \
+#               evaluation.planning_steps="$planning_steps" \
+#               evaluation.inner_planning_steps="$inner_planning_steps" \
+#               evaluation.entropy_threshold="$entropy_threshold" \
+#               evaluation.planning_mode="$planning_mode" \
+#               evaluation.planning_depth=2 \
+#               common.seed="$seed" \
+#               wandb.mode=online
 
-          done
-        done
-      done
-    done
-  done
-done
+#           done
+#         done
+#       done
+#     done
+#   done
+# done
 
 
 for env_type in Breakout; do #  ChopperCommand CrazyClimber DemonAttack Freeway Frostbite Gopher Hero Jamesbond Kangaroo Krull KungFuMaster MsPacman Pong PrivateEye Qbert RoadRunner Seaquest UpNDown
