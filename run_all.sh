15a9befe98bb0a6c99c881fa1b33d24f90a5ffd0
Labeebah. <Mona Lisa.>
2026-08-06 18:18:51 +0100

    run more seeds

--- run_all.sh
+++ run_all.sh
@@ -60,29 +60,29 @@ run_experiment() {
 #   python src/main.py env.env_type="$env_type" common.seed=0
 # done
 
-# for env_type in CoinRun; do
-#   for planning_steps in 0; do # No planning, baselines
-#     for inner_planning_steps in 0; do # No inner planning
-#       for planning_percentage in 0; do
-#         for planning_mode in value; do
-#           for seed in 0 1 2 3 4 5 6 7 8 9; do
+for env_type in CoinRun; do
+  for planning_steps in 0; do # No planning, baselines
+    for inner_planning_steps in 0; do # No inner planning
+      for planning_percentage in 0; do
+        for planning_mode in value; do
+          for seed in {10..50}; do
 
-#             run_experiment \
-#               env.env_type="$env_type" \
-#               evaluation.planning_steps="$planning_steps" \
-#               evaluation.inner_planning_steps="$inner_planning_steps" \
-#               evaluation.planning_percentage="$planning_percentage" \
-#               evaluation.planning_mode="$planning_mode" \
-#               evaluation.planning_depth=1 \
-#               common.seed="$seed" \
-#               wandb.mode=online
+            run_experiment \
+              env.env_type="$env_type" \
+              evaluation.planning_steps="$planning_steps" \
+              evaluation.inner_planning_steps="$inner_planning_steps" \
+              evaluation.planning_percentage="$planning_percentage" \
+              evaluation.planning_mode="$planning_mode" \
+              evaluation.planning_depth=1 \
+              common.seed="$seed" \
+              wandb.mode=online
 
-#           done
-#         done
-#       done
-#     done
-#   done
-# done
+          done
+        done
+      done
+    done
+  done
+done
 
 
 for env_type in CoinRun; do #StarPilot CaveFlyer Dodgeball FruitBot Chaser Miner Jumper Leaper Maze BigFish Heist Climber Plunder Ninja BossFight; do
@@ -90,7 +90,7 @@ for env_type in CoinRun; do #StarPilot CaveFlyer Dodgeball FruitBot Chaser Miner
     for inner_planning_steps in 0; do # No inner planning
       for planning_percentage in 1; do
         for planning_mode in random; do
-          for seed in 0 1 2 3 4 5 6 7 8 9; do
+          for seed in {10..50}; do
 
             run_experiment \
               env.env_type="$env_type" \
@@ -114,7 +114,7 @@ for env_type in CoinRun; do
     for inner_planning_steps in 5; do
       for planning_percentage in 0.05 0.1 0.2 0.5; do
         for planning_mode in value reward; do
-          for seed in 0 1 2; do
+          for seed in {10..50}; do
 
             run_experiment \
               env.env_type="$env_type" \
